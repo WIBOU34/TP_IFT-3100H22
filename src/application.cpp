b7bcd5710501127d39d9143f15200715da8908a6
WIBOU34 <william.boudreault.2@ulaval.ca>
2022-01-13 16:59:39 -0500

    added default methods

--- src/application.cpp
+++ src/application.cpp
@@ -26,3 +26,58 @@ void Application::draw() {
 void Application::exit() {
 	ofLog() << "<app::exit>";
 }
+
+//--------------------------------------------------------------
+void Application::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void Application::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void Application::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void Application::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void Application::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void Application::dragEvent(ofDragInfo dragInfo) {
+
+}
