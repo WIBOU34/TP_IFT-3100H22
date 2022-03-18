7066b812daf9307a528d983134ef3467bc7f7843
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2022-03-17 20:58:39 -0400

    ajout de classe texture

---
+++ src/texture/textureRenderer.cpp
@@ -0,0 +1,3 @@
+// IFT3100H21_AlmostEmptyProject/renderer.cpp
+// Classe responsable du rendu de l'application.
+
---
+++ src/texture/textureRenderer.h
@@ -0,0 +1,3 @@
+// IFT3100H21_AlmostEmptyProject/renderer.h
+// Classe responsable du rendu de l'application.
+
